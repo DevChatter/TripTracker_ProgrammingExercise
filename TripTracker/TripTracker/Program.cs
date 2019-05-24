116614b179e84d010ecfffd2211d4e0bb64f2607
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2019-05-24 13:18:35 -0400

    Initial Project Structure

---
+++ TripTracker/TripTracker/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TripTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    // Ignore Trips less than 5 MPH.
+    // Ignore Trips faster than 100 MPH.
+
+    //Sample input:
+
+    //Driver Sarah
+    //Driver Brendan
+    //Driver Michael
+    //Trip Sarah 09:15 09:45 17.3
+    //Trip Sarah 06:22 06:42 21.8
+    //Trip Brendan 12:11 13:26 42.0
+
+
+    //Sample output:
+
+    //Brendan: 42 miles @ 34 mph
+    //Sarah: 39 miles @ 47 mph
+    //Michael: 0 miles
+}
