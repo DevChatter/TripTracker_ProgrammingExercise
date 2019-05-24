ec57296cf8574718cbd89ddc48f3fad38080f135
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2019-05-24 14:52:19 -0400

    Tests for CanParse

---
+++ TripTracker/UnitTests/Core/Parsing/RecordParserTests/CanParseShould.cs
@@ -0,0 +1,57 @@
+﻿using Core.Parsing;
+using FluentAssertions;
+using Xunit;
+
+namespace UnitTests.Core.Parsing.RecordParserTests
+{
+    public class CanParseShould
+    {
+        [Theory]
+        [InlineData("Driver Brendan")]
+        [InlineData("Driver Sarah")]
+        public void ReturnTrue_GivenDriverRecordParserAndData(string line)
+        {
+            IRecordParser parser = new DriverRecordParser();
+
+            bool result = parser.CanParse(line);
+
+            result.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData("Trip Sarah 09:15 09:45 17.3")]
+        [InlineData("Trip Brendan 12:11 13:26 42.0")]
+        public void ReturnFalse_GivenDriverRecordParserAndOtherData(string line)
+        {
+            IRecordParser parser = new DriverRecordParser();
+
+            bool result = parser.CanParse(line);
+
+            result.Should().BeFalse();
+        }
+
+        [Theory]
+        [InlineData("Trip Sarah 09:15 09:45 17.3")]
+        [InlineData("Trip Brendan 12:11 13:26 42.0")]
+        public void ReturnTrue_GivenTripRecordParserAndData(string line)
+        {
+            IRecordParser parser = new TripRecordParser();
+
+            bool result = parser.CanParse(line);
+
+            result.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData("Driver Brendan")]
+        [InlineData("Driver Sarah")]
+        public void ReturnFalse_GivenTripRecordParserAndOtherData(string line)
+        {
+            IRecordParser parser = new TripRecordParser();
+
+            bool result = parser.CanParse(line);
+
+            result.Should().BeFalse();
+        }
+    }
+}
